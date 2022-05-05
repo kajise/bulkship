69976e53f6f0ea3acfac9b1d1036654758ad4fe0
Euris <teamtdm567@gmail.com>
2022-05-05 20:25:23 +0800

    🦑 octopy

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 echo Downloading Java Version Manager...
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 ## check if java is even installed correctly
-jabba install zulu@1.17.0
+jabba install openjdk@1.17.0
 java -version
