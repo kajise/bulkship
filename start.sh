7ec9ed46800947bdb27ca68fb83cc8b1727f0ae2
Euris <teamtdm567@gmail.com>
2022-05-05 09:24:36 +0800

    🐙 bump: install java 11

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 ## install jabba and sht
 echo Downloading Java Version Manager...
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## list java fckery
-jabba ls-remote
+## check if java is even installed correctly
+jabba install zulu@1.11.0
+java -version
