cae5fdeb7ed5e4fd1edb9a085b67504b720bf310
Euris <teamtdm567@gmail.com>
2022-05-05 12:39:42 +0800

    🍵 java 17

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 echo Downloading Java Version Manager...
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 ## check if java is even installed correctly
-jabba install zulu@1.11.0
+jabba install zulu@1.17.0
 java -version
