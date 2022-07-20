2e50bc5d9ab15d43758720050ee356b452223d4f
Euris <teamtdm567@gmail.com>
2022-07-21 03:32:40 +0800

    🧶 bump: new script

--- start.sh
+++ start.sh
@@ -1,6 +1,23 @@
-## install jabba and sht
-echo Downloading Java Version Manager...
-curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
-## check if java is even installed correctly
-jabba install zulu@1.11.0
-java -version
+## install jabba (java version holder) and java
+if ! [ -x "$(command -v jabba)" ]; then
+  	echo 'Error: jabba is not installed. Installing...' >&2
+  	curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
+
+	if ! [ -x "$(command -v java -version)" ]; then
+		echo 'Error: java v11 is not installed. Installing...' >&2
+		jabba install zulu@1.11.0
+		java -version
+		exit 1
+		# do your thing below
+	else
+		echo 'Success: java v11 is installed.' >&2
+		java -version
+		exit 1
+		# do your thing below
+	fi
+
+else
+	echo 'Success: jabba is installed.' >&2
+	java -version
+	exit 1
+fi
